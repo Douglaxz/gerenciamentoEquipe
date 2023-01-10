2bee780e64e7ae9f91be580503be1528206a0384
Douglas Amaral <douglaxz@hotmail.com>
2023-01-10 08:50:04 -0300

    TESTE

---
+++ gerenciador.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request
+
+class Jogo:
+    def __init__(self, nome, categoria, console):
+        self.nome = nome
+        self.categoria = categoria
+        self.console = console
+
+jogo1 = Jogo('Tetris', 'Puzzle', 'Atari')
+jogo2 = Jogo('God of war', 'Rack n Slash', 'PS2')
+jogo3 = Jogo('The Witcher 3', 'RPG', 'PS5')
+listaJogos = [jogo1, jogo2, jogo3]
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html', titulo='jogos' , jogos=listaJogos)
+
+@app.route('/novo')
+def novo():
+    return render_template('novo.html', titulo='Novo Jogo')
+
+@app.route('/criar', methods=['POST',])
+def criar():
+    nome  =request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
+    jogo = Jogo(nome, categoria, console)
+    listaJogos.append(jogo)
+    return render_template('index.html', titulo='jogos' , jogos=listaJogos)
+app.run(debug=True)
