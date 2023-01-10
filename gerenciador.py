d1d1f0f165aa663fa38017f9bf8cb65801eb71e3
Douglas Amaral <douglaxz@hotmail.com>
2023-01-10 11:33:20 -0300

    Correções da sessão do usuário

--- gerenciador.py
+++ gerenciador.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session, flash, url_for
 
 class Jogo:
     def __init__(self, nome, categoria, console):
@@ -12,6 +12,7 @@ jogo3 = Jogo('The Witcher 3', 'RPG', 'PS5')
 listaJogos = [jogo1, jogo2, jogo3]
 
 app = Flask(__name__)
+app.secret_key = 'itcio'
 
 @app.route('/')
 def index():
@@ -19,6 +20,9 @@ def index():
 
 @app.route('/novo')
 def novo():
+    if session['usuario_logado'] == None:
+        return redirect('/login?proxima=novo')
+        return redirect(url_for('login',proxima=url_for('novo')))
     return render_template('novo.html', titulo='Novo Jogo')
 
 @app.route('/criar', methods=['POST',])
@@ -28,19 +32,33 @@ def criar():
     console = request.form['console']
     jogo = Jogo(nome, categoria, console)
     listaJogos.append(jogo)
-    return redirect('/')
+    return redirect(url_for('index'))
 
 @app.route('/login')
 def login():
-    return render_template('login.html')
+    proxima = request.args.get('proxima')
+    return render_template('login.html', proxima=proxima)
 
 @app.route('/autenticar', methods = ['GET', 'POST'])
 def autenticar():
     if request.form['senha'] == "oi":
-        return redirect('/')
+        session['usuario_logado'] = request.form['usuario']
+        flash(session['usuario_logado'] + ' Usuário logado com sucesso')
+        proximaPagina = request.form['proxima']
+        if proximaPagina == "None":
+            proximaPagina = ''
+
+        return redirect('/{}'.format(proximaPagina))
+        #return redirect(proximaPagina)
     else:
-        return redirect('/login')
+        flash('Usuário não logado com sucesso')
+        return redirect(url_for('login'))
 
 
+@app.route('/logout', methods = ['GET', 'POST'])
+def logout():
+    session['usuario_logado'] = None
+    flash('logout efetuado com sucesso')
+    return redirect(url_for('login'))
 
 app.run(debug=True)
