da1ea6f4d763077bc947c4c27502fe68bb0f30b4
Douglas Amaral <douglaxz@hotmail.com>
2023-01-11 09:33:46 -0300

    Instalando as funcionalidades de editar e deletar itens

--- views.py
+++ views.py
@@ -14,9 +14,17 @@ def index():
 @app.route('/novo')
 def novo():
     if session['usuario_logado'] == None:
-        return redirect('/login?proxima=novo')
+        #return redirect('/login?proxima=novo')
         return redirect(url_for('login',proxima=url_for('novo')))
     return render_template('novo.html', titulo='Novo Jogo')
+
+# rota para criar novo formulário usuário 
+@app.route('/editar/<int:id>')
+def editar(id):
+    if session['usuario_logado'] == None:
+        return redirect(url_for('login',proxima=url_for('editar')))
+    usuario = usuarios.query.filter_by(cod_usuario=id).first()
+    return render_template('editar.html', titulo='Editando Usuário', usuario=usuario)    
 
 # rota para criar novo usuário no banco de dados
 @app.route('/criar', methods=['POST',])
@@ -25,20 +33,45 @@ def criar():
     senha = request.form['senha']
     status = request.form['status']
     login = request.form['login']
-
     usuario = usuarios.query.filter_by(nome_usuario=nome).first()
-
     if usuario:
         flash ('Usuário já existe')
-        return redirect(url_for('index'))
-    
-
+        return redirect(url_for('index')) 
     novoUsuario = usuarios(nome_usuario=nome, senha_usuario=senha, status_usuario=status, login_usuario=login )
-    
     db.session.add(novoUsuario)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+# rota para editar novo usuário no banco de dados
+@app.route('/atualizar', methods=['POST',])
+def atualizar():
+    
+    usuario = usuarios.query.filter_by(cod_usuario=request.form['id']).first()
+
+    
+    usuario.nome_usuario = request.form['nome']
+    usuario.senha_usuario = request.form['senha']
+    usuario.status_usuario = request.form['status']
+    usuario.login_usuario = request.form['login']
+
+    db.session.add(usuario)
     db.session.commit()
 
     return redirect(url_for('index'))
+
+@app.route('/deletar/<int:id>')
+def deletar(id):
+
+    if session['usuario_logado'] == None:
+        return redirect(url_for('login'))    
+    
+    
+    usuarios.query.filter_by(cod_usuario=id).delete()
+    db.session.commit()
+    flash('Usuario apagado com sucesso!')
+
+    return redirect(url_for('index'))    
+
 
 # rota para a tela de login
 @app.route('/login')
